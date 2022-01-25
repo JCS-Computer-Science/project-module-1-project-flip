3dbb1e8562c7d3cafda4c1176a4bc93dc58b79b7
Pro_fire_devlopment <34112557+ProFireDev@users.noreply.github.com>
2022-01-25 10:59:26 -0700

    Revert "Revert "fix broken build?""
    This reverts commit c3555f7807afda174b5c92b4bdde2355aff72f80.

--- Project_Flip-Vite/dist/assets/Contact.6d580b5c.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./plugin-vue_export-helper.21dcd24c.js";import{o as t,c as o}from"./vendor.63d0fee4.js";const c={},r={class:"text-center underline text-3xl font-bold"};function n(s,a){return t(),o("h1",r," This is the Contact page ")}var l=e(c,[["render",n]]);export{l as default};
